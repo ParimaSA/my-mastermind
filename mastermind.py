c461fcf216e54428dde44fae4d630dce64e3ea83
Parima SANGSIRAKOUP <parima.sa@ku.th>
2023-11-15 17:27:18 +0700

    create class and some function

--- mastermind.py
+++ mastermind.py
@@ -1,28 +1,81 @@
-check_password = '3336'
-num_color = 6
-num_position = 4
-round = 0
+import random
+import random
+import sys
 
-print(f'Playing Mastermind with {num_color} colors and {num_position} positions')
-while True:
-    password = input('What is your guess?: ')
-    print('Your guess is', password)
-    set_pass = list(set(list(password)))
-    position = 0
-    color = 0
-    for i in set_pass:
-        # print(i)
-        n = min(check_password.count(i), password.count(i))
-        color += n
-    for i in range(len(password)):
-        if password[i] == check_password[i]:
-            position += 1
-    print('*' * position, 'o' * (color - position), sep='')
-    print()
-    round += 1
 
-    if password == check_password:
-        break
+class Mastermind:
+    def __init__(self, num_color=4, num_position=6):
+        self.num_color = num_color
+        self.num_position = num_position
+        self.set_up_answer()
 
-print(f'You solve it after {round} rounds')
+    def set_up_game(self):
+        pass
+
+    def set_up_answer(self):
+        new_answer = ''
+        for i in range(self.num_position):
+            n = random.randint(1,self.num_color)
+            new_answer += str(n)
+        self.answer = new_answer
+
+    def play_game(self):
+        print(f'Playing Mastermind with {self.num_color} colors and {self.num_position} positions')
+        self.set_up_answer()
+        round = 0
+        while True:
+            position = 0
+            color = 0
+            password = input('What is your guess?: ')
+            print('Your guess is', password)
+            set_pass = list(set(list(password)))
+            for i in set_pass:
+                n = min(self.answer.count(i), password.count(i))
+                color += n
+            for i in range(len(password)):
+                if password[i] == self.answer[i]:
+                    position += 1
+            print('*' * position, 'o' * (color - position), sep='')
+            print()
+            round += 1
+            if self.answer == password:
+                break
+        print(f'You solve it after {round} rounds')
+
+        again = input('Do you want to play again (y/n)?: ')
+        while again not in ['y', 'n']:
+            again = input('Do you want to play again (y/n)?: ')
+        if again == 'y':
+            self.play_game()
+        else:
+            self.menu()
+
+    def history(self):
+        pass
+
+    def menu(self):
+        print()
+        print('Mastermind Game')
+        print('1.Play Game')
+        print('2.Set up Game')
+        print('3.Show History')
+        print('4.Exit Game')
+        ans_list = ['1', '2', '3', '4']
+        ans = input('Your Choice: ')
+        while ans not in ans_list:
+            ans = input('Your Choice: ')
+        if ans == '1':
+            self.play_game()
+        elif ans == '2':
+            self.set_up_game()
+        elif ans == '3':
+            self.history()
+        else:
+            sys.exit()
+
+my_game = Mastermind()
+my_game.menu()
+
+
+
 
